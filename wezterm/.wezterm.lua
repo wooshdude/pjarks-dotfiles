da9850910c2860bd44a7c1c3c79cb5d65122a83f
wooshdude <parker@cline.cc>
2024-09-22 01:40:10 -0700

    hyprland, wofi, waybar, kitty

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
-
+config.enable_wayland = false
 config.default_prog = { "usr/bin/zsh", "-l" }
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
